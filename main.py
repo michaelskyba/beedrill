550f9fa791dd650b342b2dd1cab6875753024e7a
MeMeBall <91382906+MeMeBallJM@users.noreply.github.com>
2024-04-17 17:37:13 -0400

    Static files

---
+++ main.py
@@ -0,0 +1,8 @@
+from config import OPENAI_API_KEY, SECRET_KEY
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
