5e5fc0def62a9f5557b1f5dd26ec73e811d85ddb
MeMeBall <91382906+MeMeBallJM@users.noreply.github.com>
2024-04-17 18:01:41 -0400

    Added route for index.html

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from config import OPENAI_API_KEY, SECRET_KEY
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, FileResponse
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/src", StaticFiles(directory="src"), name="src")
+
+@app.get("/")
+async def index_html():
+  return FileResponse('index.html')
