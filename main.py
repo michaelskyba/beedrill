71e8ab17965ee0ad8c6e691cac028eaabbf31ae4
MeMeBall <91382906+MeMeBallJM@users.noreply.github.com>
2024-04-17 17:45:49 -0400

    Added src dir to static

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/src", StaticFiles(directory="src"), name="src")
