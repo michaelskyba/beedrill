93f6823abbb7e3da2ac853863cf34a6ab0982efc
MeMeBall <91382906+MeMeBallJM@users.noreply.github.com>
2024-04-18 09:21:01 -0400

    Pushing in case I changed a name

--- decks.py
+++ decks.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 from pydantic import BaseModel
 import sqlite3
-import bcrypt
+import time
 
 from database import USER_ID, USERNAME, HASHED_PASSWORD
 
@@ -17,6 +17,11 @@ class Deck(BaseModel):
 
 class DeckId(BaseModel):
     deck_id: int
+
+
+class Card(BaseModel):
+    front: str
+    back: str
 
 
 @router.post("/decks/new")
@@ -36,6 +41,9 @@ def new_deck(request: Request, deck: Deck):
 
     return {"deck_id": cursor.lastrowid}
 
+# def is_due(repetition_number, easiness_factor, repetition_interval, last_review):
+
+
 
 @router.get("/decks/get/mine")
 def get_personal_deck(request: Request):
@@ -51,6 +59,13 @@ def get_personal_deck(request: Request):
     for deck in decks:
         deck_id = deck[0]
         deck_name = deck[2]
+
+        cursor.execute("SELECT * FROM cards WHERE deck_ud = ?;" (deck_id,))
+        cards = cursor.fetchall()
+
+        due_cards = list(filter(is_due,cards))
+
+
         json.append({"deck_id": deck_id, "deck_name": deck_name})
 
     connection.commit()
@@ -72,3 +87,53 @@ def delate_deck(deck_id: DeckId):
     connection.close()
 
     return {"deck_id": deck_id.deck_id}
+
+
+@router.post("/cards/add")
+def add_card(request: Request, deck_id: DeckId, card: Card):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+
+    current_time = int(time.time())
+
+    cursor.execute(
+        "INSERT INTO cards (deck_id, front, back, repetition_number, easiness_factor, repetition_interval, last_review) VALUES (?, ?, ?, ?, ?, ?);",
+        deck_id.deck_id,
+        card.front,
+        card.back,
+        0,
+        2.5,
+        0,
+        current_time,
+    )
+
+    connection.commit()
+
+    connection.close()
+
+# @router.get("/cards/review")
+# def review_card(request: Request, deck_id: DeckId, grade: Grade):
+#     pass
+
+
+def SM2(grade, repetition_number, easiness_factor, repetition_interval):
+    if grade >= 3:
+        if repetition_number == 0:
+            repetition_interval = 1
+        elif repetition_number == 1:
+            repetition_interval = 6
+        else:
+            repetition_interval = round(repetition_interval * easiness_factor)
+        repetition_number += 1
+    else:
+        repetition_number = 0
+        repetition_interval = 1
+    easiness_factor = easiness_factor + (0.1 - (5 - grade) * (0.08 + (5 - grade) * 0.02))
+    if easiness_factor < 1.3:
+        easiness_factor = 1.3
+
+    return (repetition_number, easiness_factor, repetition_interval)
+        
+            
+
+
