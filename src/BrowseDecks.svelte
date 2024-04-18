2fbb2f9429b3504d428a5e5a7a1dcc5699380b58
Michael Skyba <michaelskyba@net-c.com>
2024-04-18 10:38:17 -0400

    Empty BrowseDecks

--- src/BrowseDecks.svelte
+++ src/BrowseDecks.svelte
@@ -1,2 +1,19 @@
 <script>
+	import { page, myDecks } from "./store.js";
+
+	async function getDecks() {
+		return;
+
+		const response = await fetch("http://127.0.0.1:8000/decks/get/public");
+		const data = await response.json();
+
+		const updatedDecks = data.map(deck => {
+			return {
+				id: deck.deck_id,
+				name: deck.deck_name,
+				due_cards: deck.due_card_count,
+			}
+		});
+		myDecks.set(updatedDecks);
+	}
 </script>
