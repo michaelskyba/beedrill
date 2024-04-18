a058a6aa10ef5be8c23f9cd66f5de6f95f02d040
Michael Skyba <michaelskyba@net-c.com>
2024-04-17 20:53:40 -0400

    Show username in Home

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,4 +1,14 @@
 <script>
+	import { user } from "./store.js";
 </script>
 
-<p>Welcome to Beedrill.</p>
+<p>
+	Welcome to Beedrill.
+
+	{#if $user}
+		You are logged in as <code>{$user.username}</code>.
+	{:else}
+		You are logged out.
+	{/if}
+</p>
+
